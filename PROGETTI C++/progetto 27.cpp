f6b3cdc39589de2de3af98ec567b70dee8455ee1
kirbhoo cockburg <antoniomuntean98@gmail.com>
2024-05-27 08:36:59 +0000

    Prove in classe

---
+++ PROGETTI C++/progetto 27.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace::std;
+
+int main(){
+
+    cout << " BENVENUTO SU GIT POD! " <<endl;
+    string name;
+    cout << " SCRIVI IL TUO NOME" << endl;
+    cin >> name;
+    cout << " CIAO" << " " << name << endl;
+
+
+
+    return 0;
+}
